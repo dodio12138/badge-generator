b1f90bc3306d7f0ddab357552d3354e6e0890e35
dodio12138 <1159715241@qq.com>
2023-08-28 00:00:59 +0100

    fixed bugs

--- src/js/index.js
+++ src/js/index.js
@@ -15,9 +15,28 @@ generateButton.addEventListener('click', generateBadge);
 // 获取输入框和按钮的元素
 const fileInput = document.getElementById('fileInput');
 const changeButton = document.getElementById('logo-upload-button');
-const logoInput = document.getElementById('logo-input')
+const logoInput = document.getElementById('logo-input');
+const copyMD = document.getElementById('copy-md');
+const copyHTML = document.getElementById('copy-html');
+let MDCode = '';
+let HTMLCode = '';
 let svg = '';
+let badgeURL = 'https://img.shields.io/static/v1?label=Dodio&message=badge-generator&color=blue&logo=github';
 // 添加按钮的点击事件监听器
+copyMD.addEventListener('click', function () {
+    if (navigator.clipboard) {
+        MDCode = `![Static Badge](${badgeURL})`
+        navigator.clipboard.writeText(MDCode);
+    }
+});
+
+copyHTML.addEventListener('click', function () {
+    if (navigator.clipboard) {
+        HTMLCode = `<img alt="Static Badge" src="${badgeURL}">`
+        navigator.clipboard.writeText(HTMLCode);
+    }
+});
+
 changeButton.addEventListener('click', function () {
     // 切换输入框的显示状态
     if (fileInput.style.display === 'none') {
@@ -42,21 +61,41 @@ async function generateBadge() {
             style =  styleChoose.id;
         }
     });
-    const logoColor = document.getElementById('logo-color-input').value || '';
-    const label = document.getElementById('label-input').value || '';
-    const labelColor = document.getElementById('label-color-input').value || '';
-    const link = document.getElementById('link-input').value || '';
+    let logoColor = '';
+    let label = '';
+    let labelColor = '';
+    let link = '';
     let logo = '';
-    let badgeURL = '';
+
+    if(document.getElementById('logo-color-input').value != ''){
+        logoColor = "&logoColor=" + document.getElementById('logo-color-input').value;
+    }
+    if(document.getElementById('label-input').value != ''){
+        label = "&label=" + document.getElementById('label-input').value;
+    }
+    if(document.getElementById('label-color-input').value != ''){
+        labelColor = "&labelColor=" + document.getElementById('label-color-input').value;
+    }
+    if(document.getElementById('link-input').value != ''){
+        link = "&link=" + document.getElementById('link-input').value;
+    }
+    // const logoColor = document.getElementById('logo-color-input').value || '';
+    // const label = document.getElementById('label-input').value || '';
+    // const labelColor = document.getElementById('label-color-input').value || '';
+    // const link = document.getElementById('link-input').value || '';
+    // let logo = '';
+    // let badgeURL = '';
     if(fileInput.style.display === 'none'){
-        logo = document.getElementById('logo-input').value || ' ';
-        badgeURL = `https://img.shields.io/badge/${content}-${color}?style=${style}&logo=${logo}&logoColor=${logoColor}&label=${label}&labelColor=${labelColor}&link=${link}`;
+        if(document.getElementById('logo-input').value != ''){
+            logo = "&logo=" + document.getElementById('logo-input').value;
+        }
+        badgeURL = `https://img.shields.io/badge/${content}-${color}?style=${style}${logo}${logoColor}${label}${labelColor}${link}`;
     }else if (fileInput.style.display === 'block'){
         console.log(svg2Base64());
         await svg2Base64();
         logo = svg;
         console.log(svg2Base64());
-        badgeURL = `https://img.shields.io/badge/${content}-${color}.svg?logo=data:image/svg%2bxml;base64,${logo}&style=${style}&logoColor=${logoColor}&label=${label}&labelColor=${labelColor}&link=${link}`;
+        badgeURL = `https://img.shields.io/badge/${content}-${color}.svg?logo=data:image/svg%2bxml;base64,${logo}&style=${style}${logoColor}${label}${labelColor}${link}`;
     }
     // 构建Shields.io链接
     // const badgeURL = `https://img.shields.io/badge/${content}-${color}?style=${style}&logo=${logo}&logoColor=${logoColor}&label=${label}&labelColor=${labelColor}&link=${link}`;
@@ -71,7 +110,7 @@ async function generateBadge() {
     if(link==''){
         badgeShowLink.href = badgeURL;
     }else{
-        badgeShowLink.href = link;
+        badgeShowLink.href = "https://" + link.replace("&link=",'');
     }
     badgeShowLink.title = 'staticBadge';
 
